6d4ccbc36b3604b92532246d58ea44e547a083e6
Vinay <vinaykumar979@gmail.com>
2017-05-22 22:08:33 +0530

    Paginator

--- src/app/school/school.component.ts
+++ src/app/school/school.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {SchoolService} from '../services/school.service';
-import {AutoCompleteModule} from 'primeng/primeng';
+import {AutoCompleteModule, PaginatorModule} from 'primeng/primeng';
+
 
 
 
@@ -20,12 +21,13 @@ text: string;
 country: any;    
 countries: any[];        
 filteredCountriesSingle: any[];
-
+schoolsData: any[];
 constructor(private schoolService : SchoolService) {
 this.showFilter=false;
 
 this.schoolService.getData().subscribe((data) => {
-  console.log(data.schools[0].addresses["0"].address);
+  console.log(data.schools);
+  this.schoolsData= data.schools;
 // alert(data.school.[0].addresses.city);
   this.schoolData=data;
 });
@@ -66,4 +68,14 @@ toggleFilter()
   this.showFilter=true;
    }
 }
+
+paginate(onPageChange) {
+    console.log("Page changed");
+        //event.first = Index of the first record
+        //event.rows = Number of rows to display in new page
+        //event.page = Index of the new page
+        //event.pageCount = Total number of pages
+    }
+
+
 }
--- src/app/school/school.module.ts
+++ src/app/school/school.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SchoolComponent } from './school.component';
-import {DialogModule,AutoCompleteModule} from 'primeng/primeng';
+import {DialogModule,AutoCompleteModule,PaginatorModule} from 'primeng/primeng';
 import {FormsModule} from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     AutoCompleteModule,
-    FormsModule
+    FormsModule,
+    PaginatorModule
   ],
   declarations: [SchoolComponent]
 })
