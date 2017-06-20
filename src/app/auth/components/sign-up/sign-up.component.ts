7264a49930b1417bd7585eda0b43ff396e8dad5e
Vinay <vinaykumar979@gmail.com>
2017-06-20 14:14:39 +0530

    Vallidation

--- src/app/auth/components/sign-up/sign-up.component.ts
+++ src/app/auth/components/sign-up/sign-up.component.ts
@@ -82,20 +82,6 @@ export class SignUpComponent implements OnInit, OnDestroy {
     });
   }
 
-  
-
-/*static MatchPassword(AC: AbstractControl) {
-       let password = AC.get('password').value; // to get value in input tag
-       let confirmPassword = AC.get('confirmPassword').value; // to get value in input tag
-        if(password != confirmPassword) {
-            console.log('false');
-            AC.get('confirmPassword').setErrors( {MatchPassword: true} )
-        } else {
-            console.log('true');
-            return null
-        }
-    }
-*/
 
   
 
