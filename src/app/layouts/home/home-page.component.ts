cad6b53d5a2d36c9303752033dc510f536a48159
Yudhi Satrio <isatrio.is.me@gmail.com>
2017-07-07 10:40:02 +0800

    [refs #00000] delete all files

--- src/app/layouts/home/home-page.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-//import { Router, NavigationEnd } from '@angular/router';
-
-@Component({
-  selector: 'app-layout',
-  templateUrl: './home-page.component.html'
-})
-export class HomePageComponent {
-}
