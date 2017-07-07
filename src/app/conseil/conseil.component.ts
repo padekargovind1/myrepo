bbcb9c32fa3c09bb683a3c141a96c98bf50d9166
Marc-Hu <marc.hu@outlook.fr>
2017-07-07 14:29:13 +0800

    add conseil component

--- src/app/conseil/conseil.component.ts
+++ src/app/conseil/conseil.component.ts
@@ -8,7 +8,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ConseilComponent implements OnInit {
 
-  constructor(private route : ActivatedRoute) { }
+  constructor() { }
 
   ngOnInit() {
   }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  Title : String = "Ma Recherche";
+
+  constructor(private router : Router) { }
 
   ngOnInit() {
+  }
+
+  onNavigateHome(){
+    this.router.navigate( ['/'] );
   }
 
 }
