7c77c8eb0442f6c4b3c3d90b0714f73c6cd1f1b1
Yudhi Satrio <isatrio.is.me@gmail.com>
2017-07-11 09:53:50 +0800

    [refs #00000] Implement fullcalendar

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.component.html',
@@ -10,6 +11,7 @@ export class BookingComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  	// $('#calendar').fullCalendar();
   }
 
 }
