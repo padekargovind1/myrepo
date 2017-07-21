7f570c37921a79e6a5038093c53a662e2fb7e32b
Yudhi Satrio <isatrio.is.me@gmail.com>
2017-07-21 14:25:53 +0800

    [refs #00000] Fixed conseil color

--- src/app/landing-page-1/landing-page-1.component.ts
+++ src/app/landing-page-1/landing-page-1.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page-1',
   templateUrl: './landing-page-1.component.html',
   styleUrls: ['./landing-page-1.component.scss']
 })
-export class LandingPage1Component implements OnInit {
+export class LandingPage1Component implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+  	(<any> $('.landingpage__additional-content')).on('mouseover', function() {
+
+        (<any> $('.landingpage__additional-content .form-wrap')).removeClass('fadeIn').addClass('fadeOut');
+        (<any> $('.landingpage__additional-content')).css('z-index', 0);
+        
+        (<any> $(this)).css('z-index', 10);
+        (<any> $(this)).find('.form-wrap').removeClass('fadeOut').addClass('fadeIn');
+        var img = (<any> $(this)).data('background'); console.log(img);
+
+        (<any> $('body .main')).css({ 'background-image': 'url(' + img + ')', 'background-size': 'cover'});
+    });
   }
 
 }
