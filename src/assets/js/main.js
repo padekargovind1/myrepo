7879fccad64b79c7f781f449eb54265be3d6ac68
rohit-gys <rohit-gys@users.noreply.github.com>
2017-08-09 11:45:45 +0530

    Multiselct Trackduck #60
    Fixed issues by updating the js file

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -36,8 +36,7 @@ jQuery(document).ready(function($) {
     });
 
     $('.school-carousel').slick();
-	
-	$(".mat-tab-label-container .mat-tab-label").click(function(){
+	$(document).on('click', ".mat-tab-label-container .mat-tab-label", function(){
 		if($(".mat-tab-body-wrapper >.mat-tab-body-active").find(".select-custom-multiple select").length>0)
 		{
 			$(".mat-tab-body-wrapper >.mat-tab-body-active").find(".select-custom-multiple select").select2({
@@ -45,13 +44,4 @@ jQuery(document).ready(function($) {
 			});
 		}
 	});
-
-    $(".mat-tab-label-container .mat-tab-label").click(function(){
- 		if($(".mat-tab-body-wrapper >.mat-tab-body-active").find(".select-custom-multiple select").length>0)
- 		{
- 			$(".mat-tab-body-wrapper >.mat-tab-body-active").find(".select-custom-multiple select").select2({
- 				placeholder: 'Matières fortes'
- 			});
- 		}
- 	});
 });
