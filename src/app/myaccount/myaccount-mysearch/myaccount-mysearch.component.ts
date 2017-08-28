cd6a6c8b3b6005878ff3145c30444783a715773b
rohit-gys <rohit-gys@users.noreply.github.com>
2017-08-28 13:17:14 +0530

    Date Sorting TrackDuck #120, 121
    I have implemented the sorting on the button click

--- src/app/myaccount/myaccount-mysearch/myaccount-mysearch.component.ts
+++ src/app/myaccount/myaccount-mysearch/myaccount-mysearch.component.ts
@@ -11,6 +11,9 @@ export class MyaccountMysearchComponent implements OnInit {
   wishList = [];
   applyList = [];
   historyList = [];
+  wishAsc: boolean = false;
+  applyAsc: boolean = false;
+  historyAsc: boolean = false;
   constructor(private usersService : UsersService) { }
 
   ngOnInit() {
@@ -65,6 +68,47 @@ export class MyaccountMysearchComponent implements OnInit {
           }
         }
       )
+  }
+  custom_sort_asc(a, b) {
+	return new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime();
+  }
+  custom_sort_desc(a, b) {
+	return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
+  }
+
+  onSortClick(sortType){
+	switch(sortType){
+		case "wish" : 
+			if(this.wishAsc){ 
+				this.wishList = this.wishList.sort(this.custom_sort_desc); 
+				this.wishAsc=false;
+			} 
+			else{ 
+				this.wishList = this.wishList.sort(this.custom_sort_asc);				
+				this.wishAsc=true;
+			}
+			break;
+		case "apply" : 
+			if(this.applyAsc){ 
+				this.applyList = this.applyList.sort(this.custom_sort_desc); 
+				this.applyAsc=false;
+			} 
+			else{ 
+				this.applyList = this.applyList.sort(this.custom_sort_asc);				
+				this.applyAsc=true;
+			}
+			break;
+		case "history" : 
+			if(this.historyAsc){ 
+				this.historyList = this.historyList.sort(this.custom_sort_desc); 
+				this.historyAsc=false;
+			} 
+			else{ 
+				this.historyList = this.historyList.sort(this.custom_sort_asc);				
+				this.historyAsc=true;
+			}
+			break;
+	}
   }
 
 }
