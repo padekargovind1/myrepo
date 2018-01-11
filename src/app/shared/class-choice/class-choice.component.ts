ca5f4955f610c224e478be97672f447836fba4c4
harish <hmahajan.dmi@gmail.com>
2018-01-11 21:27:11 +0530

    TD #493 fixed

--- src/app/shared/class-choice/class-choice.component.ts
+++ src/app/shared/class-choice/class-choice.component.ts
@@ -2,6 +2,7 @@
 import { MdDialogRef, MD_DIALOG_DATA } from '@angular/material';
 import { Router } from '@angular/router';
 import { SchoolService } from '../../services/school.service';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-class-choice',
@@ -12,7 +13,7 @@ export class ClassChoiceComponent implements OnInit {
 
   schoolData : any;
   schoolIndex : any;
-
+  selectValue:boolean;
   constructor(public dialogref:MdDialogRef<ClassChoiceComponent>,
               @Inject(MD_DIALOG_DATA) private data: {schoolData : any, schoolIndex : any},
               private router : Router,
@@ -24,12 +25,27 @@ export class ClassChoiceComponent implements OnInit {
     this.schoolIndex=this.data.schoolIndex;
   }
 
+  onChange() {
+    this.selectValue = true;
+  }
   onselect(index){
-    this.router.navigate(['applyto', this.schoolData._id]);
-    this.schoolService.onApplyTo(true);
-    this.schoolService.storeClassName(this.schoolData.cycles[this.schoolIndex].classes[index]._id)
-    console.log(this.schoolData, this.schoolData.cycles[this.schoolIndex].classes[index]._id)
-    this.dialogref.close()
+    if(this.selectValue){
+        this.selectValue = false;
+        this.router.navigate(['applyto', this.schoolData._id]);
+        this.schoolService.onApplyTo(true);
+        this.schoolService.storeClassName(this.schoolData.cycles[this.schoolIndex].classes[index]._id)
+        console.log(this.schoolData, this.schoolData.cycles[this.schoolIndex].classes[index]._id)
+        this.dialogref.close()
+    }
+    else {
+        this.selectValue = false;
+        swal({
+              title: 'Please select value of dropdown',
+              text: '',
+              type: 'success',
+              confirmButtonText: 'Ok'
+            });
+    }
   }
 
   navigateBack(){
