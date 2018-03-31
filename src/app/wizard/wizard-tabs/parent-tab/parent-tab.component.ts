da22fd88c948977fed45356be1ad689ea9b5eed6
harish <hmahajan.dmi@gmail.com>
2018-03-31 14:53:52 +0530

    sc-60 fixed

--- src/app/wizard/wizard-tabs/parent-tab/parent-tab.component.ts
+++ src/app/wizard/wizard-tabs/parent-tab/parent-tab.component.ts
@@ -1,8 +1,10 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {FormArray, FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {CustomValidators} from "ng2-validation";
-import {WizardService} from "../../../services/wizard.service";
-import {BookingService} from "../../../services/booking.service";
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators, FormControl } from "@angular/forms";
+import { CustomValidators } from "ng2-validation";
+import { WizardService } from "../../../services/wizard.service";
+import { BookingService } from "../../../services/booking.service";
+declare const jquery: any;
+declare const $: any;
 
 @Component({
   selector: 'app-parent-tab',
@@ -10,90 +12,89 @@ import {BookingService} from "../../../services/booking.service";
   styleUrls: ['./parent-tab.component.css']
 })
 export class ParentTabComponent implements OnInit {
-  wizardForm : FormGroup;
-  parents : any;
-  onMobile:boolean=false;
+  wizardForm: FormGroup;
+  parents: any;
+  onMobile: boolean = false;
   lienparents = [];
- 
+
   @Input() userData;
   @Output() tabChange: EventEmitter<number> = new EventEmitter<number>();
-
-  constructor(private fb : FormBuilder,
-              private wizardService : WizardService,
-              private bookingService : BookingService) { }
+  constructor(private fb: FormBuilder,
+    private wizardService: WizardService,
+    private bookingService: BookingService) { }
 
   ngOnInit() {
     this.buildForm();
     this.getLienParent();
- 
+
   }
 
-  buildForm(){
-    this.wizardForm=this.fb.group({
-      parents : this.fb.array([this.createParent()])
+  buildForm() {
+    this.wizardForm = this.fb.group({
+      parents: this.fb.array([this.createParent()])
     })
-    setTimeout(()=>{
+    setTimeout(() => {
       this.patchValue()
     }, 1000)
   }
   //Create a form group of parent
-  createParent(){
+  createParent() {
     return this.fb.group({
-      lienParent : ['', Validators.required],
-      titre : ['', Validators.required],
-      nom : ['',Validators.required],
-      prenom : ['', Validators.required],
-      job : ['', Validators.required],
-      email : ['', Validators.compose([CustomValidators.email, Validators.required])],
-      portable : ['', Validators.compose([Validators.required, Validators.maxLength(10)])],
-      horaireJoignable : ['', Validators.required]
+      lienParent: ['', Validators.required],
+      titre: ['', Validators.required],
+      nom: ['', Validators.required],
+      prenom: ['', Validators.required],
+      job: ['', Validators.required],
+      email: ['', Validators.compose([CustomValidators.email, Validators.required])],
+      portable: ['', Validators.compose([Validators.required, Validators.maxLength(10)])],
+      horaireJoignable: ['', Validators.required]
     })
   }
   //Patch value from userData
-  patchValue(){
-    var IsAdress = (this.userData.address!==undefined && this.userData.address!=null) ? true : false;
-    for (let i = 0; i<this.wizardForm.controls['parents']['controls'].length; i++){
-      if(this.userData.parents!==undefined && this.userData.parents!=null && this.userData.parents.length!=0){
-        IsAdress = (this.userData.parents[i].address!==undefined && this.userData.parents[i].address!=null) ? true : false;
+  patchValue() {
+    var IsAdress = (this.userData.address !== undefined && this.userData.address != null) ? true : false;
+    for (let i = 0; i < this.wizardForm.controls['parents']['controls'].length; i++) {
+      if (this.userData.parents !== undefined && this.userData.parents != null && this.userData.parents.length != 0) {
+        IsAdress = (this.userData.parents[i].address !== undefined && this.userData.parents[i].address != null) ? true : false;
         this.wizardForm.controls['parents']['controls'][i].patchValue({
-          lienParent : this.userData.parents[i].relationship,
-          titre : this.userData.parents[i].gender,
-          nom : this.userData.parents[i].lastName,
-          prenom : this.userData.parents[i].firstName,
-          email : this.userData.parents[i].email,
-          portable : this.userData.parents[i].phoneNumber,
-          adresse : IsAdress ? this.userData.parents[i].address.address1 : "",
-          codepostal : IsAdress ? this.userData.parents[i].address.postCode : "",
-          ville : IsAdress ? this.userData.parents[i].address.city : "",
-          pays : IsAdress ? this.userData.parents[i].address.country : "",
-          job : this.userData.parents[i].profession,
+          lienParent: this.userData.parents[i].relationship,
+          titre: this.userData.parents[i].gender,
+          nom: this.userData.parents[i].lastName,
+          prenom: this.userData.parents[i].firstName,
+          email: this.userData.parents[i].email,
+          portable: this.userData.parents[i].phoneNumber,
+          adresse: IsAdress ? this.userData.parents[i].address.address1 : "",
+          codepostal: IsAdress ? this.userData.parents[i].address.postCode : "",
+          ville: IsAdress ? this.userData.parents[i].address.city : "",
+          pays: IsAdress ? this.userData.parents[i].address.country : "",
+          job: this.userData.parents[i].profession,
           horaireJoignable: this.userData.parents[i].availability
         })
       }
     }
   }
   //Add a parent
-  onAddParent(){
+  onAddParent() {
     this.parents = this.wizardForm.get('parents') as FormArray;
     this.parents.push(this.createParent());
   }
   //Remove a parent
-  onRemoveParent(index){
+  onRemoveParent(index) {
     this.parents = this.wizardForm.get('parents') as FormArray;
     this.parents.removeAt(index, 1);
   }
   //Save and go to the next tab
-  nextTab(nb : number){
-    if(this.wizardForm.valid){
+  nextTab(nb: number) {
+    if (this.wizardForm.valid) {
       //console.log("123");
       this.wizardService.saveData('parentData', this.wizardForm.value.parents);
       this.tabChange.emit(nb);
-    }else {
+    } else {
       // if(this.lienParent)
       //   this.lienParentErrorMsg= false;
       // else
       //   this.lienParentErrorMsg= true;
- 
+
       // if(this.wizardForm.value.parents.lienParent === undefined || this.wizardForm.value.parents.lienParent === 'undefined' || this.wizardForm.value.parents[0].lienParent===undefined || this.wizardForm.value.parents[0].lienParent === '')
       //   this.lienParentErrorMsg= true;
       // else
@@ -121,7 +122,7 @@ export class ParentTabComponent implements OnInit {
     }
   }
 
-  getLienParent(){
-    this.lienparents=this.wizardService.getLienParent();
+  getLienParent() {
+    this.lienparents = this.wizardService.getLienParent();
   }
 }
